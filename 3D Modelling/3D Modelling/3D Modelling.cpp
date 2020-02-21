9f6daaf5fafe0a32de98840340b9fa1e9cd8caa7
Alec <crazyatblay@gmail.com>
2020-02-21 15:14:52 +0000

    Updated Project so libraries work.

--- 3D Modelling/3D Modelling/3D Modelling.cpp
+++ 3D Modelling/3D Modelling/3D Modelling.cpp
@@ -1,42 +1,45 @@
-//#include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
 
-//#include "lib/glut/GL/glut.h" CHECK THIS?
-#include "GL/glew.h"
-#include "GLFW/glfw3.h"
-
-#pragma comment(lib, "assimp-vc140-mt")
+//#include "lib/glut/GL/glut.h" need to re-add glut/GLFW
 
 #include <assimp/Importer.hpp>
-#include <assimp/Exporter.hpp>
-//#include <assimp/cimport.h>
-//#include <assimp/cexport.h>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
-#include <list>
-
 using namespace std;
-//using namespace Assimp;
+using namespace Assimp;
 
 
 int main()
-{    
-    glfwInit();
-    GLFWwindow* window = glfwCreateWindow(1080, 720, "Fabirc Physics", NULL, NULL);
-    glfwMakeContextCurrent(window);
-    glewInit();
+{
+	/*glfwInit();
+	GLFWwindow* window = glfwCreateWindow(1080, 720, "Fabirc Physics", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	glewInit();*/
+	//glutInit();
 
-    Assimp::Importer importer;
-    
-    //const aiScene* scene = NULL;
-    
-    string path = "";
+	const aiScene* scene = NULL;
 
-    //scene = importer.ReadFile(path,aiProcess_Triangulate);
-        
+	string path = "";
+	Importer importer;
+	scene = importer.ReadFile(path, aiProcess_Triangulate);
+	//scene = importer.GetScene();
+
+	//scene = aiImportFile(path.c_str,aiProcessPreset_TargetRealtime_MaxQuality);
+
+
+
+
+	Assimp::Importer importer;
+
+	//const aiScene* scene = NULL;
+
+	string path = "";
+
+	//scene = importer.ReadFile(path,aiProcess_Triangulate);
+
    // Exporter exporter;
-    //size_t formatCount = aiGetExportFormatCount();
+	//size_t formatCount = aiGetExportFormatCount();
    // const list<aiExportFormatDesc>* types;
    // const aiExportFormatDesc* format;
    // for (int i = 0; i < (int)formatCount; i++)
@@ -44,8 +47,9 @@ int main()
   //    format = aiGetExportFormatDescription(i);
   //  }
 
-    //exporter.Export(scene, format->description, "testName", aiPostProcessSteps::aiProcess_Triangulate, NULL);
+	//exporter.Export(scene, format->description, "testName", aiPostProcessSteps::aiProcess_Triangulate, NULL);
 
 //    aiExportScene(scene,aiGetExportFormatDescription() ,"textFile");
-    
+
+
 }
