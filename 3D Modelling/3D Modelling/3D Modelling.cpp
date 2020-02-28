75c27af5e37f34aeb7adfb9af9e83d89a683e72c
Alec <crazyatblay@gmail.com>
2020-02-28 16:13:06 +0000

    Fixed path saving/loading working: using local files, in pure obj format, will change to different formats once moved away from fixed input/output paths.

--- 3D Modelling/3D Modelling/3D Modelling.cpp
+++ 3D Modelling/3D Modelling/3D Modelling.cpp
@@ -8,10 +8,10 @@
 
 #include "GL/glut.h"
 #include <assimp/cimport.h>
-//#include <assimp/cexport.h>
+#include <assimp/cexport.h>
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
-#include <list>
+#include <vector>
 using namespace std;
 using namespace Assimp;
 
@@ -27,7 +27,7 @@ int main()
 	string path = "C:\\Users\\crazy\\OneDrive\\Documents\\Assimp\\teapot.obj";
 
 	const aiScene* scene = aiImportFile(path.c_str(), aiProcessPreset_TargetRealtime_MaxQuality);
-	list<aiMesh*> meshList;
+	vector<aiMesh*> meshList;
 	for (int i = 0; i < scene->mNumMeshes; i++)
 	{
 
@@ -38,7 +38,7 @@ int main()
 	//list<aiVector3D> verticies;
 	//meshes.push_back(scene->mMeshes);
 
-	list<aiVector3D*> verticiesList;
+	vector<aiVector3D*> verticiesList;
 
 	for (int i = 0; i < meshList.size(); i++)
 	{
@@ -47,47 +47,59 @@ int main()
 		if (meshList.front()->mNumVertices > 0)
 		{
 			aiMesh* temp = meshList.front();
-			
+
 			//verticiesList.push_front(meshList.front()->mVertices);
 			for (int j = 0; j < temp->mNumVertices; j++)
 			{
-				verticiesList.push_front(&temp->mVertices[j]);
+				verticiesList.push_back(&temp->mVertices[j]);
+				//printf("%f,%f, %f\n", verticiesList[j]->x, verticiesList[j]->y, verticiesList[j]->z);
 			}
-			printf("home");
-			
+			//printf("home");
+
 		}
 		//float xVal = verticies->x;
 
 		//printf("%f", xVal);
 	}
 
+	///Test operation
+	for (int test = 0; test < verticiesList.size(); test++)
+	{
+		aiVector3D* val = verticiesList[test];
+		aiVector3D addTest(val->x, val->y, val->z);
+		addTest = addTest + addTest;
 
+		val->x = addTest.x;
+		val->y = addTest.y;
+		val->z = 0;//addTest.z;
+		verticiesList[test] = val;
+	}
 
+	//scene->mMeshes[0]->mVertices;
 
+	size_t exportNumbers = aiGetExportFormatCount();
+	const char* desc;
+	printf("Avaliable formats");
+	for (int i = 0; i < exportNumbers; i++)
+	{
+		desc = aiGetExportFormatDescription(i)->description;
+		printf(desc);
+		printf("\n");
+	}
+	desc= aiGetExportFormatDescription(4)->description;
+	aiReturn status = aiExportScene(scene, aiGetExportFormatDescription(4)->id, "C:\\Users\\crazy\\OneDrive\\Documents\\Assimp\\flat.obj", aiProcess_Triangulate);
+	if (status == AI_SUCCESS)
+	{
+		printf("\nSuccessfully saved\n");
+	}
+	else
+	{
+		printf("\nError saving.\n");
+	}
+	
+	
 	aiReleaseImport(scene);
 
 	aiDetachAllLogStreams();
-
-
-
-	//const aiScene* scene = NULL;
-
-	//string path = "";
-
-	//scene = importer.ReadFile(path,aiProcess_Triangulate);
-
-   // Exporter exporter;
-	//size_t formatCount = aiGetExportFormatCount();
-   // const list<aiExportFormatDesc>* types;
-   // const aiExportFormatDesc* format;
-   // for (int i = 0; i < (int)formatCount; i++)
-  //  {
-  //    format = aiGetExportFormatDescription(i);
-  //  }
-
-	//exporter.Export(scene, format->description, "testName", aiPostProcessSteps::aiProcess_Triangulate, NULL);
-
-	//aiExportScene(scene,aiGetExportFormatDescription() ,"textFile");
-
 
 }
